86355c892e451eda95488e1fdcc5b47fad4d1859
drogonly1 <drogonly1@gmail.com>
2021-06-26 10:59:34 +0700

    â

---
+++ Lab2/Lab2/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The Complete Manual - Author Name Book");
+            books.Add("HTML5 & CSS Responsive web Design cookbook - Author Name Book 2");
+            books.Add("Professional ASP.NET MVC5 - Author Name Book 2");
+            ViewBag.Books = books;
+            return View();
+        }
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The Complete Manual", "Author Name Book 1", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3egfEcqpoHfgfhIU_3ztvulREW5CZDZp9fQ&usqp=CAU"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author Name Book 2", "https://i.pinimg.com/236x/f0/e9/74/f0e974ec44ac81dbfd88b665b0c4ebd0--html.jpg"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author Name Book 3", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk42hqvW2OWgUgIcI47NYUTRQJC_Np5heTHw&usqp=CAU"));
+            return View(books);
+        }
+        public ActionResult EditBook(int id, string Title, string Author, string ImageCover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The Complete Manual", "Author Name Book 1", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3egfEcqpoHfgfhIU_3ztvulREW5CZDZp9fQ&usqp=CAU"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author Name Book 2", "https://i.pinimg.com/236x/f0/e9/74/f0e974ec44ac81dbfd88b665b0c4ebd0--html.jpg"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author Name Book 3", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk42hqvW2OWgUgIcI47NYUTRQJC_Np5heTHw&usqp=CAU"));
+            Book book = new Book();
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+            }
+            return View("ListBookModel",books);
+        }
+    }
+}
---
+++ Lab2/Lab2/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+        public Book()
+        {
+
+        }
+        public Book(int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string ImageCover { get => image_cover; set => image_cover = value; }
+    }
+}
